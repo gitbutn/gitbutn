1bb222df0d41de6885f2475be1043e81cb09b7fe
saleh jarad <salehjarad@salehs-iMac.local>
2018-09-06 06:06:47 +0300

    lovley lets try it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './MainApp';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
