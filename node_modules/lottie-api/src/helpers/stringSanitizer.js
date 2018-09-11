930b726e1a48a5df9a0f375f25eef3914adf2775
saleh jarad <salehjarad@salehs-iMac.local>
2018-09-11 12:29:11 +0300

    updating project

--- node_modules/lottie-api/src/helpers/stringSanitizer.js
+++
@@ -1,5 +0,0 @@
-function sanitizeString(string) {
-	return string.trim();
-}
-
-module.exports = sanitizeString
--- node_modules/lottie-api/src/layer/effects/EffectElement.js
+++
@@ -1,8 +0,0 @@
-var Property = require('../../property/Property');
-
-function EffectElement(effect, parent) {
-
-	return Property(effect.p, parent);
-}
-
-module.exports = EffectElement;
